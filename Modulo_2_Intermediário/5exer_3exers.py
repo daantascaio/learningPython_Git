ad8393139f4c3e2d673396219358bae79bc73967
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-09 21:51:28 -0300

    only saving somethings

--- Modulo_2_Intermediário/5exer_3exers.py
+++ Modulo_2_Intermediário/5exer_3exers.py
@@ -1,7 +1,6 @@
 # copy, sorted, produtos.sort
 # Exercícios
-# Aumente os preços dos produtos a seguir em 10%
-# Gere novos_produtos por deep copy (cópia profunda)
+
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -9,9 +8,6 @@ produtos = [
     {'nome': 'Produto 2', 'preco': 105.87},
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
-
-# Ordene os produtos por nome decrescente (do maior para menor)
-# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
@@ -33,8 +29,37 @@ produtos = [
 
 # print(aumenta_10porcento(novos_precos))
 
-for i in produtos:
-    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+2) Ordene os produtos por nome decrescente (do maior para menor)
+   Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+"""
+
 
 
 
