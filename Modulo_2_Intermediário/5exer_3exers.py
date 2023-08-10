72d9bc8818d8d20f62af4b7642a587c8767c6b23
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-10 13:49:33 -0300

    exercise 1 finished

--- Modulo_2_Intermediário/5exer_3exers.py
+++ Modulo_2_Intermediário/5exer_3exers.py
@@ -1,3 +1,6 @@
+import copy
+import os
+
 # copy, sorted, produtos.sort
 # Exercícios
 
@@ -8,9 +11,6 @@ produtos = [
     {'nome': 'Produto 2', 'preco': 105.87},
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
-
-# Ordene os produtos por preco crescente (do menor para maior)
-# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
 """
 1) Aumente os preços dos produtos a seguir em 10%
@@ -33,47 +33,101 @@ produtos = [
 #     preco_alterado = i['preco']
 #     print(preco_alterado)
 
-def values_second_index_dictionaries(product_list):
-    for dictionary in product_list:
-        yield dictionary['preco']
+# def values_second_index_dictionaries(product_list):
+#     for dictionary in product_list:
+#         yield dictionary['preco']
 
-def increase_10_percent(numbers):
-    result = []
-    for num in numbers:
-        num = num * 1.1
-        num = "{:.2f}".format(num)
-        result.append(float(num))
-    yield list(result)
+# def increase_10_percent(numbers):
+#     result = []
+#     for num in numbers:
+#         num = num * 1.1
+#         num = "{:.2f}".format(num)
+#         result.append(float(num))
+#     return result
 
-values_without_increase = list(values_second_index_dictionaries(produtos))
-print(f'VALOR ORIGINAL: {values_without_increase}')
+# def check_price():
 
-values_whith_increase = increase_10_percent(values_without_increase)
-print(f'VALUE WITH INCREASE 10%: {values_whith_increase}')
+#     while True:
+#         price = input("Digite o preço do produto: ")
+
+#         try:
+#             price = float(price)
+          
+#         except ValueError:
+#             print("Preço inválido. Digite um preço correto!")
+#             continue
+
+#         return price
+
+# def create_products(name_product, price_product):
+
+#     product_dict = {'nome': name_product, 'preco': price_product}
+
+#     return product_dict
+
+# values_without_increase = list(values_second_index_dictionaries(produtos))
+# print(f'VALOR ORIGINAL: {values_without_increase}')
+
+# values_whith_increase = list(increase_10_percent(values_without_increase))
+# print(f'VALUE WITH INCREASE 10%: {values_whith_increase}')
 
 
 
+# while True:
+#     cat = input('Digite [i] para adicionar produtos na lista ou não digite nada para parar o sistema: ')
+
+#     if cat == 'i':
+
+#         name_product = input('Digite o nome do produto: ')
+#         price_product = check_price()
+        
+#         new_product = create_products(name_product, price_product)
+
+#         products_copy = copy.deepcopy(produtos)
+
+#         products_copy.append(new_product)
+#         print(products_copy)
+#         continue
+#     os.system('cls')
+#     break
+
+# print(f'\nLISTA ORIGINAL')
 
 
+# for i in produtos:
+#     nome = i['nome']
+#     preco = i['preco']
+#     print(f"Nome: {nome}, Preço: {preco}")
+
+# print(f'\nLISTA DEEPCOPY')
+
+# for i in products_copy:
+#     nome = i['nome']
+#     preco = i['preco']
+#     print(f"Nome: {nome}, Preço: {preco}")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# s = input('\nPressione a tecla [Enter] para continuar! ')
+# os.system('cls')
 """
 2) Ordene os produtos por nome decrescente (do maior para menor)
    Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 """
 
 
+print(produtos.sort)
 
+
+
+
+
+
+
+
+
+
+"""
+3) Ordene os produtos por preco crescente (do menor para maior)
+   Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+"""
 
