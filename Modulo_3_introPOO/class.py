c6c93040d124d965173ae9033bb111822bfb5de7
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-17 16:34:38 -0300

    continuing

--- Modulo_3_introPOO/class.py
+++ Modulo_3_introPOO/class.py
@@ -9,18 +9,22 @@ print(string.upper())
 print(isinstance(string, str))
 """
 
+# __init__ --> inicializa os atributos da minha classe
 
 class People:
-    ...
+    def __init__(self, firstName, secondName):
+        self.firstName = firstName
+        self.secondName = secondName
+        pass
 
 
-people_one = People()
-people_one.firstName = 'Caio'
-people_one.secondName = 'Dantas'
+people_one = People('Caio', 'Dantas')
+# people_one.firstName = 'Caio'
+# people_one.secondName = 'Dantas'
 
-people_two = People()
-people_two.firstName = 'Isadora'
-people_two.secondName = 'Fiorentino'
+people_two = People('Isadora', 'Fiorentino')
+# people_two.firstName = 'Isadora'
+# people_two.secondName = 'Fiorentino'
 
 print(people_one.firstName)
 print(people_one.secondName)
@@ -29,4 +33,6 @@ print()
 
 print(people_two.firstName)
 print(people_two.secondName)
+
+
 
