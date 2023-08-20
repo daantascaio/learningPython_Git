31bee3c6ef26dadb4492b043523fc6c3b17f8652
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-19 22:16:06 -0300

    about __dict__ and vars

--- Modulo_3_introPOO/__disct__--_vars.py
+++ Modulo_3_introPOO/__disct__--_vars.py
@@ -4,18 +4,27 @@ class People:
 
     ano_atual = 2023
 
-    def __init__(self, name, age):
+    def __init__(self, name, age): # the name, age and other attributes to be added are saved in __dict__
         self.name = name
         self.age = age
 
     def get_age_borny(self):
         return People.ano_atual - self.age
     
-people_one = People('Caio', 20)
+
+data_Caio = {'name': 'Caio', 'age': 23}
+
+people_one = People(**data_Caio)
+print(vars(people_one))
+
 # people_two = People('Isadora', 21)
 # people_tre = People('Cecília', 1)
 # print(people_one.get_age_borny())
 # print(people_two.get_age_borny())
 # print(people_tre.get_age_borny())
 
-print(people_one.__dict__)
+# people_one.__dict__['height'] = 1.83
+# people_one.__dict__['age'] = 23
+# print(people_one.__dict__)
+# print(vars(people_one))
+# print(people_one.height)
