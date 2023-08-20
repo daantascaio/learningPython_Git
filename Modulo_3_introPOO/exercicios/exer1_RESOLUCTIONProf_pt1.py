f306b7746e70b143174329fa049bfe31ccb839cb
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-20 11:23:58 -0300

    __name__

--- Modulo_3_introPOO/exercicios/exer1_RESOLUCTIONProf_pt1.py
+++ Modulo_3_introPOO/exercicios/exer1_RESOLUCTIONProf_pt1.py
@@ -14,7 +14,13 @@ class Pessoa:
 
 p1 = Pessoa('Caio', 33)
 p2 = Pessoa('Helena', 21)
-bd = [vars(p1), __dict__(p2)]
+p3 = Pessoa('João', 47)
+bd = [vars(p1), vars(p2), vars(p3)]
 
-with open(PATH_FILE, 'w') as arquivo:
-    json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+def fazer_dump():
+    with open(PATH_FILE, 'w') as arquivo:
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+if __name__ == '__main__':
+    print('ELE É O __main__')
+    fazer_dump()
