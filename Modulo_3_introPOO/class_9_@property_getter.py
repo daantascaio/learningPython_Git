2756c5b575c0a12514edf4b9edce5c4659daa490
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-22 16:56:16 -0300

    usando um getter

--- Modulo_3_introPOO/class_9_@property_getter.py
+++ Modulo_3_introPOO/class_9_@property_getter.py
@@ -2,12 +2,32 @@
 # getter - um método para obter um atributo
 # cor -> get_cor()
 # modo pythônico - modo do Python de fazer coisas
+
 # @property é uma propriedade do objeto, ela
-# é um método que se comporta como um
-# atributo 🤯 🤯 🤯
+# é um método que se comporta como um atributo 🤯 🤯 🤯
+
 # Geralmente é usada nas seguintes situações:
 # - como getter
 # - p/ evitar quebrar código cliente
 # - p/ habilitar setter
 # - p/ executar ações ao obter um atributo
+
 # Código cliente - é o código que usa seu código
+
+class Caneta:
+    
+    def __init__(self, cor):
+        #private #protected #public ----> protegendo meus atributos
+        self.cor_tinta = cor
+
+    def get_cor(self):
+        print('GET COR')
+        return self.cor_tinta
+
+
+caneta = Caneta('Azul')
+print(caneta.get_cor())
+print(caneta.get_cor())
+print(caneta.get_cor())
+print(caneta.get_cor())
+print(caneta.get_cor())
