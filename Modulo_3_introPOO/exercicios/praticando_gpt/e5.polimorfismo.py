d911f02a9885bb87406ced49a3add96708945673
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-24 17:59:17 -0300

    iniciando o exercicio 5

---
+++ Modulo_3_introPOO/exercicios/praticando_gpt/e5.polimorfismo.py
@@ -0,0 +1,5 @@
+"""
+Exercício 5: Polimorfismo
+Crie uma classe abstrata chamada Animal com um método abstrato fazerSom(). 
+Em seguida, crie classes derivadas como Cachorro, Gato e Vaca, cada uma implementando o método fazerSom() de maneira única.
+"""
