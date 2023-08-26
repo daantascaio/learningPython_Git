23e39d5b8ac798b28e46435297e9becf583a381e
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-26 09:26:39 -0300

    startando herança, bora que o bglh é louco

--- Modulo_3_introPOO/heranca.py
+++ Modulo_3_introPOO/heranca.py
@@ -8,3 +8,31 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar_nome_classe(self):
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+
+class Cliente(Pessoa):
+    ...
+
+class Aluno(Pessoa):
+    ...
+
+
+c1 = Cliente('Caio', 'Dantas')
+c1.falar_nome_classe()
+a1 = Aluno('João', 'Doido')
+a1.falar_nome_classe()
+# help(Cliente)
+
+
+
+
+
+
+
