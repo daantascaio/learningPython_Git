e64c1855ea94878db091d1b317680aab185b2f5c
Caio Dantas <caiodantasl.lara@gmail.com>
2023-08-27 10:34:32 -0300

    herança

--- Modulo_3_introPOO/super_sub.py
+++ Modulo_3_introPOO/super_sub.py
@@ -3,8 +3,6 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
-
-
 # class MinhaString(str):
 #     def upper(self):
 #         print('CHAMOU UPPER')
@@ -12,10 +10,11 @@
 #         print('DEPOIS DO UPPER')
 #         return retorno
 
+
 # string = MinhaString('Luiz')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'valor a'
 
     def __init__(self, atributo):
@@ -24,18 +23,42 @@ class A:
     def metodo(self):
         print('A')
 
+
 class B(A):
     atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
     def metodo(self):
         print('B')
 
+
 class C(B):
     atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        # print('EI, burlei o sistema.')
+        super().__init__(*args, **kwargs)
+
     def metodo(self):
-        super(B, self).metodo()
-        super(C, self).metodo()
+        # super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        A.metodo(self)
+        B.metodo(self)
         print('C')
 
-c = C('atributo')
-print(c.atributo_a, c.atributo_b, c.atributo_c)
+
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer')
+# print(c.atributo)
+# print(c.outra_coisa)
 c.metodo()
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# c.metodo()
