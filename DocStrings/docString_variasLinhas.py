79a435bd480b075fcd0d951265470cdf09c98213
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-01 17:20:56 -0300

    docstrings de várias linahs

---
+++ DocStrings/docString_variasLinhas.py
@@ -0,0 +1,25 @@
+"""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nulla 
+tempor, pulvinar orci nec, facilisis dolor. Fusce mattis, elit in tincidunt 
+mattis, nisi eros imperdiet ipsum, eget molestie nibh urna in justo. 
+Nulla faucibus, ante in euismod fermentum, velit dolor congue turpis, et rhoncus
+ipsum diam eu nulla. Integer mauris orci, convallis in mauris non, tincidunt 
+consequat lacus. Nunc aliquam dapibus sem, sagittis blandit est tincidunt 
+maximus. Curabitur faucibus risus id justo dignissim molestie id eu lacus. 
+Integer porttitor elementum nibh, in malesuada leo suscipit id. 
+Integer ut pharetra est. Mauris et pulvinar lacus, in eleifend ligula. 
+Nullam ac libero leo. Vestibulum ante ipsum primis in faucibus orci luctus et 
+ultrices posuere cubilia curae; Nam tempus, neque eget egestas faucibus, libero
+urna elementum est, id vulputate arcu elit sed mauris. Sed tempus massa sit 
+amet nulla condimentum, sed luctus magna volutpat. Donec sit amet mauris eget
+nunc bibendum viverra.
+
+Suspendisse pharetra odio orci, rhoncus bibendum justo semper sit amet. Morbi 
+nunc justo, dictum non neque ut, malesuada gravida est. Donec lacinia cursus 
+vehicula. Integer lorem ligula, auctor ac rutrum vitae, hendrerit at risus. 
+Donec lacinia vehicula ligula. Praesent ultrices elit quam, ut consequat quam 
+euismod sit amet. In tempor augue lectus, quis placerat orci sodales aliquam. 
+Sed ac purus tellus. Sed gravida, turpis et laoreet ornare, orci magna venenatis
+est, et viverra massa nunc vel odio. Sed eu mi sed nisi malesuada mollis vel 
+nec enim. Ut non nisl felis.
+"""
--- DocStrings/main.py
+++ DocStrings/main.py
@@ -1,7 +1,11 @@
-import docString_umaLinha
+# import docString_umaLinha
 
-print(dir(docString_umaLinha))
-print(docString_umaLinha.__doc__)
-print(docString_umaLinha.__file__)
-print(docString_umaLinha.__name__)
-help(docString_umaLinha)
+# print(dir(docString_umaLinha))
+# print(docString_umaLinha.__doc__)
+# print(docString_umaLinha.__file__)
+# print(docString_umaLinha.__name__)
+# help(docString_umaLinha)
+
+import docString_variasLinhas
+
+help(docString_variasLinhas)
