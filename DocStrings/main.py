909517c148541ee70b85365617b06fef5b776208
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-01 14:54:42 -0300

    aprendendo sobre DocStrings!

---
+++ DocStrings/main.py
@@ -0,0 +1,7 @@
+import docString_umaLinha
+
+print(dir(docString_umaLinha))
+print(docString_umaLinha.__doc__)
+print(docString_umaLinha.__file__)
+print(docString_umaLinha.__name__)
+help(docString_umaLinha)
