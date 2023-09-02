8725bcdaf538d52b759be433e750da0992096b7a
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-02 17:49:11 -0300

    terminando as criação das classes Cp e Cc

--- Modulo_3_introPOO/exercicios/exer3_solucao/contas.py
+++ Modulo_3_introPOO/exercicios/exer3_solucao/contas.py
@@ -1,7 +1,7 @@
 import abc
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -11,7 +11,7 @@ class Conta(abc.ABC):
 
     def depositar(self, valor):
         self.saldo += valor
-        self.detalhes(f'(DEPÓSITO) {valor}')
+        self.detalhes(f'(DEPÓSITO - {valor})')
 
     def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f} {msg}')
@@ -21,7 +21,50 @@ class ContaPoupanca(Conta):
     def sacar(self, valor):
         valor_pos_saque = self.saldo - valor
 
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
         
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO - {valor})')
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo=0, limite=0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+        
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+        
+        print('Não foi possível sacar o valor desejado')
+        print(f'Seu limite é {-self.limite:.2f} | SAQUE DISPONÍVEL {-self.limite - self.saldo:.2f}')
+        self.detalhes(f'(SAQUE NEGADO - {valor})')
+
+if __name__ == '__main__':
+    print('TESTE POUPANÇA ^^^')
+    conta_poupança1 = ContaPoupanca(111,222)
+    conta_poupança1.sacar(1)
+    conta_poupança1.depositar(100)
+    conta_poupança1.sacar(99)
+    print('###############')
+    conta_corrente1 = ContaCorrente(111,222,0,100)
+    conta_corrente1.sacar(1)
+    conta_corrente1.depositar(100.99)
+    conta_corrente1.sacar(102.46)
+    conta_corrente1.sacar(3)
+    conta_corrente1.sacar(5)
+    conta_corrente1.sacar(70)
+    conta_corrente1.sacar(17.37)
+    conta_corrente1.sacar(20.37)
+    
 
 
     
