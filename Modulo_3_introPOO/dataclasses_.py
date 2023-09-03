67bbb9c57d6f2d45d943eea0d82ddadeeb59c5ed
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-03 09:15:18 -0300

    continuando dataclasses_

--- Modulo_3_introPOO/dataclasses_.py
+++ Modulo_3_introPOO/dataclasses_.py
@@ -3,7 +3,7 @@
 # __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo
 # usuário.
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
-# Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
+# Foi descrito na PEP 557 e adicionado na versão 3.7s do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
 
 from dataclasses import dataclass
@@ -11,12 +11,29 @@ from dataclasses import dataclass
 @dataclass
 class Pessoa:
     nome: str
-    idade: int
+    sobrenome: str
+
+    @property
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome }'
+    
+    @nome_completo.setter
+    def nome_completo(self, valor: str):
+        nome, *sobrenome = valor.split()
+        self.nome = nome
+        self.sobrenome = ' '.join(sobrenome)
+
+
 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Caio', 30)
+    p1 = Pessoa('Caio', 'Dantas')
     print(p1)
+    print(p1.nome_completo)
+    p1.nome_completo = 'Dore Durado'
+    print(p1.nome_completo)
+    p1.nome_completo = 'Dore Durado Durou Duro'
+    print(p1.nome_completo)
 
 
 
