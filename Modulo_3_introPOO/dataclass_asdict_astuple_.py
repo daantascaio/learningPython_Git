c1d771e4f29c6ee68ce0a1e719079131ad798d54
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-03 10:02:23 -0300

    field e fileds

--- Modulo_3_introPOO/dataclass_asdict_astuple_.py
+++ Modulo_3_introPOO/dataclass_asdict_astuple_.py
@@ -10,11 +10,10 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
-    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
-    print(ordenadas)
     p1 = Pessoa('Luiz', 'Otávio')
     print(asdict(p1).keys())
     print(asdict(p1).values())
     print(asdict(p1).items())
     print(astuple(p1)[0])
+
+    
